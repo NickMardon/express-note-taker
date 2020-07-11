216c22b06ee412c1e70118fb37287ac87f253696
Nick Mardon <nickmardondigital@gmail.com>
2020-07-10 21:59:25 -0700

    api post is loging to console, but not writing to db

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,17 +1,25 @@
 const express = require("express");
 const path = require("path");
+const fs = require('fs');
+const bodyParser = require("body-parser");
+const {Note, deleteNote} = require('./../db/db');
+// const app = express();
+// MIDDLEWARE
+// app.use(bodyParser.json());
 const router = express.Router();
 
-
 // Get Request
-router.get("/api/notes", function(req, res) {
+router.get("/api/notes",(req, res)=>{
     res.sendFile(path.join(__dirname,'../db/db.json'))
   });
 
-// // Post Request
-// router.get("/api/notes", function(req, res) {
-//     return res.send('../db/db.json')
-//   });
+// Post Request
+router.post("/api/notes", async (req, res)=>{
+   const note = req.body;
+   console.log(note);
+  });
+
+
 
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const path = require("path");
+const fs = require('fs');
 const app = express();
 
 var PORT = process.env.PORT || 3000;
