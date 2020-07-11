7fc68f972257e2b9f89a67fd7b62a723436c802f
Nick Mardon <nickmardondigital@gmail.com>
2020-07-10 17:38:09 -0700

    api get request is working

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,12 +3,15 @@ const path = require("path");
 const router = express.Router();
 
 
-// Get Requests
+// Get Request
+router.get("/api/notes", function(req, res) {
+    res.sendFile(path.join(__dirname,'../db/db.json'))
+  });
+
+// // Post Request
 // router.get("/api/notes", function(req, res) {
 //     return res.send('../db/db.json')
 //   });
-
-
 
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require("express");
-
+const path = require("path");
 const app = express();
 
 var PORT = process.env.PORT || 3000;
 
 // MIDDLEWARE
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
